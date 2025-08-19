33b9f257bc72f47a6b51027774d8d08dd21354d7
Ali Faris <alihcompiler@gmail.com>
2025-08-19 18:12:29 +0300

    initing react app

---
+++ UnoReact/vitest.config.js
@@ -0,0 +1,25 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    test: {
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: ['./TestSetup.ts'],
+        include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+        exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'json', 'html'],
+            exclude: ['node_modules/', 'src/TestSetup.ts', '**/*.d.ts', '**/*.config.{js,ts}', 'dist/'],
+        },
+    },
+});
