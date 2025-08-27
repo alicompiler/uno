226de890950e3db7aca957e5ad20523a189db359
Maytham Mahdi <metham553@gmail.com>
2025-08-27 16:35:29 +0300

    update base URL in GamesService to include port 3000

--- UnoReact/src/Domain/Game/GamesService.ts
+++ UnoReact/src/Domain/Game/GamesService.ts
@@ -1,4 +1,4 @@
-const baseUrl = `http://${window.location.hostname}`;
+const baseUrl = `http://${window.location.hostname}:3000`;
 import Axios from 'axios';
 import type { GameStatus } from '../Message/Incoming/GameStatusMessagePayload';
 
