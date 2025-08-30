5abb61fdf31e52a939780ace2646f8a7489df779
Ali Faris <alihcompiler@gmail.com>
2025-08-31 00:44:16 +0300

    fix input placeholder issue

--- UnoReact/src/Components/TextInput/TextInput.tsx
+++ UnoReact/src/Components/TextInput/TextInput.tsx
@@ -16,7 +16,6 @@ export const TextInput: React.FC<Props> = (props) => {
                 type="text"
                 disabled={props.disabled}
                 value={props.value}
-                placeholder="Enter the game id"
                 onChange={(e) => props.onChange(e.target.value)}
                 className="w-80 border border-yellow-400 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:bg-gray-700"
             />
